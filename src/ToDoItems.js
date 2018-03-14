4c4416073609e72a167da21003820319241a4c9e
shefmeister <shefmeister@users.noreply.github.com>
2018-03-14 10:59:57 -0700

    Displaying Lists
    Displaying Lists

--- src/ToDoItems.js
+++ src/ToDoItems.js
@@ -1,2 +1,21 @@
 import React, {Component} from "react";
 
+class ToDoItems extends Component {
+
+    createTasks(task){
+        return (
+            <li key = {task.key}>{task.text}</li>
+        )
+    }
+    render(){
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map(this.createTasks);
+        return(
+            <ul className="theList">
+                {listItems}
+            </ul>
+        );
+    }
+};
+
+export default ToDoItems;
